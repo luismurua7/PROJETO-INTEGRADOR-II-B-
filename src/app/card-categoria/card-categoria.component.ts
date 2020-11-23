32a5fa7880471c7a0ba9d3517683e0e3b10ee49d
luismurua7 <luismarcosmurua@gmail.com>
2020-11-23 11:51:06 -0300

    2 files modified, 3 added

---
+++ src/app/card-categoria/card-categoria.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "card-categoria",
+  templateUrl: "./card-categoria.component.html",
+  styleUrls: ["./card-categoria.component.css"]
+})
+export class CardCategoriaComponent {}
