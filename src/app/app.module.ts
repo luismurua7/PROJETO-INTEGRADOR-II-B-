56a609f5d074d6ea475b0904fdf74eb6a711ec2e
GabrielZimmer <gabrielzimmer2011@hotmail.com>
2020-11-24 17:03:17 -0300

    fire modules add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { AngularFireModule } from "@angular/fire";
 import { FormsModule } from "@angular/forms";
 import { Routes } from "@angular/router";
-
-import { environment } from "./environments/environment";
 import { AppComponent } from "./app.component";
+
+//firebase
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAnalyticsModule } from "@angular/fire/analytics";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { environment } from "./environments/environment";
+//fimfirebase
+
+//pages
 import { ProdDescrComponent } from "./pages/details/prod-descr/prod-descr.component";
 import { CardCategoriaComponent } from "./pages/inicio/card-categoria/card-categoria.component";
+//fimpages
 
 const routes: Routes = [
   { path: "", component: CardCategoriaComponent }, // inicio
@@ -15,7 +22,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
   declarations: [AppComponent, CardCategoriaComponent, ProdDescrComponent],
   bootstrap: [AppComponent]
 })
