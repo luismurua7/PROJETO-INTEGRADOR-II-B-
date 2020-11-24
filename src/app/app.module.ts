6527d3ec577e87df8b885d16ba1a68cdbd380c13
luismurua7 <luismarcosmurua@gmail.com>
2020-11-24 19:37:55 -0300

    3 files modified, 3 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 
-//firebase
-import { environment } from "./environments/environment";
-//fimfirebase
-
-//pages
 import { ProdDescrComponent } from "./pages/details/prod-descr/prod-descr.component";
 import { CardCategoriaComponent } from "./components/card-categoria/card-categoria.component";
 import { ListaCategoriasComponent } from "./pages/lista-categorias/lista-categorias.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { HeaderComponent } from "./components/header/header.component";
-//fimpages
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule],
---
+++ src/app/pages/lista-produtos/lista-produtos.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "lista-produtos",
+  templateUrl: "./lista-produtos.component.html",
+  styleUrls: ["./lista-produtos.component.css"]
+})
+export class ListaProdutosComponent {}
