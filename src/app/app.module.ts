19e779419ee3ae2978c9dfc05989d5acad190e3f
luismurua7 <luismarcosmurua@gmail.com>
2020-11-23 21:02:31 -0300

    2 files modified, 6 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-
 import { AppComponent } from "./app.component";
-import { CardCategoriaComponent } from "./card-categoria/card-categoria.component";
+import { Routes } from "@angular/router";
+import { ProdDescrComponent } from "./pages/details/prod-descr/prod-descr.component";
+import { CardCategoriaComponent } from "./pages/inicio/card-categoria/card-categoria.component";
+
+const routes: Routes = [
+  { path: "", component: CardCategoriaComponent }, // inicio
+  { path: "details", component: ProdDescrComponent } // produto
+];
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, CardCategoriaComponent],
+  declarations: [AppComponent, CardCategoriaComponent, ProdDescrComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/pages/details/prod-descr/prod-descr.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "prod-descr",
+  templateUrl: "./prod-descr.component.html",
+  styleUrls: ["./prod-descr.component.css"]
+})
+export class ProdDescrComponent {}
