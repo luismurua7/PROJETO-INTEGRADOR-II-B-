59933e5ee3708b4089dffb74a191b6da15f3a098
luismurua7 <luismarcosmurua@gmail.com>
2020-11-25 19:48:40 -0300

    3 files modified

--- src/app/components/prod-descr/prod-descr.component.ts
+++ src/app/components/prod-descr/prod-descr.component.ts
@@ -1,8 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "prod-descr",
   templateUrl: "./prod-descr.component.html",
   styleUrls: ["./prod-descr.component.css"]
 })
-export class ProdDescrComponent {}
+export class ProdDescrComponent {
+  @Input() public nomeProduto: string;
+  @Input() public imgSrc: string;
+  @Input() public descricao: string;
+}
