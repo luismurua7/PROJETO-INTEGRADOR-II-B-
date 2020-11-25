f42aa4f5d372a340a2ff13737c6348b2c021d751
luismurua7 <luismarcosmurua@gmail.com>
2020-11-25 18:56:50 -0300

    3 files deleted

--- src/app/components/prod-list/prod-list.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "prod-list",
-  templateUrl: "./prod-list.component.html",
-  styleUrls: ["./prod-list.component.css"]
-})
-export class ProdListComponent {}
